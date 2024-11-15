686ace23aa23f95b3f2156486ed29feed407686b
ffiiy <ffiiy@ANNA>
2024-11-15 18:37:25 +0200

    Add project files.

---
+++ c2c3/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace c2c3.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
